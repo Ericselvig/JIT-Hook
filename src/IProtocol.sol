e864d14084f844c8025d07056221b818f213d973
naman mohnani <mohnaninaman@gmail.com>
2024-11-23 00:35:26 +0530

    interface of external protocol

---
+++ src/IProtocol.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+interface IProtocol {
+    
+    function deposit() external {}
+    function withdraw() external {}
+    function redeemYeild() external {}
+}
--- src/JITHook.sol
+++ src/JITHook.sol
@@ -32,4 +32,35 @@ contract JITHook is BaseHook {
                 afterRemoveLiquidityReturnDelta: false
             });
     }
+
+    // if swap is big => remove lquidity from external protocol and add liquidity 
+    function beforeSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, bytes calldata)
+        external
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        return (this.beforeSwap.selector, BeforeSwapDelta(0, 0), 0);
+    }
+
+    // remove liquidity from pool and add to external protocol
+    function afterSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        virtual
+        returns (bytes4, int128)
+    {
+        return (this.afterSwap.selector, 0);
+    }
+
+    // smaller LPs will call this function, funds added to external protocol
+    function deposit() external {}
+
+    // funds transferred to small LPs from external protocol
+    function withdraw() external {}
+
+    // redeem what ? yeild tokens from external protocol OR swap fees ?
+    function redeem() external {}
+
+    function _depositToExternalProtocol() internal {}
+
+    function _withdrawFromExternalProtocol() internal {}
 }

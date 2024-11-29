b61737021ad4c487eb04a4d91f474c763fb999c2
Yash Goyal <yashgoyaldev@gmail.com>
2024-11-30 00:25:49 +0530

    Made Governance Token soulbound

--- src/governance/GovToken.sol
+++ src/governance/GovToken.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import {Owned} from "solmate/src/auth/Owned.sol";
 
 contract GovToken is ERC20, Owned {
+    error Not_Allowed();
+
     constructor(
         string memory name,
         string memory symbol
@@ -16,5 +18,27 @@ contract GovToken is ERC20, Owned {
 
     function burn(address from, uint256 amount) external onlyOwner {
         _burn(from, amount);
+    }
+
+    function approve(
+        address spender,
+        uint256 amount
+    ) public override returns (bool) {
+        revert Not_Allowed();
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        revert Not_Allowed();
+    }
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        revert Not_Allowed();
     }
 }

52e45221218157b95e5eadb03b3ba2eb69a6267d
Yash Goyal <yashgoyaldev@gmail.com>
2024-11-29 17:20:03 +0530

    added helper functions to remove stack too deep error but structure needs modification

--- src/JITHook.sol
+++ src/JITHook.sol
@@ -20,14 +20,14 @@ import {TickMath} from "v4-core/src/libraries/TickMath.sol";
 import {GovToken} from "./governance/GovToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {Owned} from "solmate/src/auth/Owned.sol";
 
-contract JITHook is BaseHook {
+contract JITHook is BaseHook, Owned {
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
     using BeforeSwapDeltaLibrary for BeforeSwapDelta;
 
-    AggregatorV3Interface public token0PriceFeed;
-    AggregatorV3Interface public token1PriceFeed;
+    mapping(Currency currency => AggregatorV3Interface priceFeed) priceFeeds;
 
     uint256 public swapThreshold; // swap threshold in USD (8 decimals)
     IStrategiesController public controller;
@@ -42,15 +42,18 @@ contract JITHook is BaseHook {
         IPoolManager _manager,
         address _strategiesController,
         uint256 _threshold,
-        address _token0PriceFeed,
-        address _token1PriceFeed,
         address _posManager
-    ) BaseHook(_manager) {
+    ) BaseHook(_manager) Owned(msg.sender) {
         controller = IStrategiesController(_strategiesController);
         swapThreshold = _threshold;
-        token0PriceFeed = AggregatorV3Interface(_token0PriceFeed);
-        token1PriceFeed = AggregatorV3Interface(_token1PriceFeed);
         positionManager = IPositionManager(_posManager);
+    }
+
+    function setPriceFeed(
+        Currency currency,
+        address priceFeed
+    ) external onlyOwner {
+        priceFeeds[currency] = AggregatorV3Interface(priceFeed);
     }
 
     function getHookPermissions()
@@ -110,91 +113,41 @@ contract JITHook is BaseHook {
         // 1. detect big swap
         // 2. remove liquidity from external protocol
         // 3. add liquidity to pool
-
-        uint256 precision;
-        int256 price;
-        int256 amountSpecified = params.amountSpecified;
-        uint256 token0Decimals = ERC20(Currency.unwrap(key.currency0))
-            .decimals();
-        uint256 token1Decimals = ERC20(Currency.unwrap(key.currency1))
-            .decimals();
-
-        if (params.zeroForOne) {
-            if (params.amountSpecified < 0) {
-                precision = 10 ** token0Decimals;
-                amountSpecified = -amountSpecified;
-                (, price, , , ) = token0PriceFeed.latestRoundData();
-            } else {
-                precision = 10 ** token1Decimals;
-                (, price, , , ) = token1PriceFeed.latestRoundData();
-            }
-        } else {
-            if (params.amountSpecified < 0) {
-                amountSpecified = -amountSpecified;
-                precision = 10 ** token1Decimals;
-                (, price, , , ) = token1PriceFeed.latestRoundData();
-            } else {
-                precision = 10 ** token0Decimals;
-                (, price, , , ) = token0PriceFeed.latestRoundData();
-            }
+        if (msg.sender == address(this)) {
+            return (
+                this.beforeSwap.selector,
+                BeforeSwapDeltaLibrary.ZERO_DELTA,
+                0
+            );
         }
 
-        // this should be in USD (8 decimals)
-        uint256 amountToSwap = (uint256(amountSpecified) * uint256(price)) /
-            precision;
-
-        if (amountToSwap >= swapThreshold) {
+        if (_getSwapAmount(key, params) >= swapThreshold) {
             // current price ratio
-            (, int24 tick, , ) = StateLibrary.getSlot0(poolManager, key.toId());
-            uint160 sqrtPriceX96 = TickMath.getSqrtPriceAtTick(tick);
-            uint256 currentPoolPrice = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96) * 1e18) >> 192;
 
-            // getting token balance deposit to external protocol (strategy balance)
-            uint256 token0Balance;
-            uint256 token1Balance;
-            (token0Balance, token1Balance) = _getBalanceFromStrategy(currentActiveStrategyId, key.currency0, key.currency1);
-
-            // check if we have enough funds to cover the swap before adding liquidity
-            int256 absAmountSpecified = amountSpecified < 0 ? -amountSpecified : amountSpecified;
-            require(token0Balance >= uint256(absAmountSpecified) || token1Balance >= (uint256(absAmountSpecified) * currentPoolPrice) / 1e18, "No balance");
-
-            // withdraw funds from external swap before adding to the pool
-            uint256 amountWithdrawn0 = _withdrawFromStrategy(currentActiveStrategyId, key.currency0, token0Balance);
-            uint256 amountWithdrawn1 = _withdrawFromStrategy(currentActiveStrategyId, key.currency1, token1Balance);
-
-            uint256 requiredAmount0AccordingToCurrentPoolRatio = (token1Balance * 1e18) / currentPoolPrice;
-            uint256 requiredAmount1AccordingToCurrentPoolRatio = (token0Balance * currentPoolPrice) / 1e18;
-
-            // check if internal swap is required before adding liquidity
-            if(token0Balance > requiredAmount0AccordingToCurrentPoolRatio) {
-                // swap token0 for token1
-                uint256 excessAmount0 =  token0Balance - requiredAmount0AccordingToCurrentPoolRatio;
-                poolManager.swap(
-                    key, 
-                    IPoolManager.SwapParams({
-                        zeroForOne: true, 
-                        amountSpecified: int256(excessAmount0), 
-                        sqrtPriceLimitX96: 0
-                    }),
-                    ""
+            if (!_checkIfHookHasEnoughBalance(key, params)) {
+                return (
+                    this.beforeSwap.selector,
+                    BeforeSwapDeltaLibrary.ZERO_DELTA,
+                    0
                 );
-            } else if (token1Balance > requiredAmount1AccordingToCurrentPoolRatio) {
-                // swap token1 for token0
-                uint256 excessAmount1 =  token1Balance - requiredAmount1AccordingToCurrentPoolRatio;
-                poolManager.swap(
-                    key, 
-                    IPoolManager.SwapParams({
-                        zeroForOne: false, 
-                        amountSpecified: int256(excessAmount1), 
-                        sqrtPriceLimitX96: 0
-                    }),
-                    ""
-                );
-
             }
 
+            // withdraw funds from external swap before adding to the pool
+
+            // check if internal swap is required before adding liquidity
+            (
+                uint256 requiredAmount0AccordingToCurrentPoolRatio,
+                uint256 requiredAmount1AccordingToCurrentPoolRatio
+            ) = _swap(key);
+
             // add liquidity to pool
-            _addLiquidityToPool(key, 0, 0, uint128(requiredAmount0AccordingToCurrentPoolRatio), uint128(requiredAmount1AccordingToCurrentPoolRatio));
+            _addLiquidityToPool(
+                key,
+                0,
+                0,
+                uint128(requiredAmount0AccordingToCurrentPoolRatio),
+                uint128(requiredAmount1AccordingToCurrentPoolRatio)
+            );
         }
 
         return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
@@ -267,15 +220,14 @@ contract JITHook is BaseHook {
     }
 
     // funds transferred to small LPs from external protocol
-    // note what if when the user withdraw pair of tokens, that ratio is changed 
+    // note what if when the user withdraw pair of tokens, that ratio is changed
     // we accept deposit in proper ratio so we must withdraw in current ratio as well
-    // INTERNAL SWAP 
+    // INTERNAL SWAP
     function withdraw(
         uint256 amount0,
         uint256 amount1,
         PoolKey calldata key
     ) external {
-
         GovToken govToken = govTokens[key.toId()];
         uint256 userBalance = govToken.balanceOf(msg.sender);
         uint256 totalSupply = govToken.totalSupply();
@@ -283,10 +235,13 @@ contract JITHook is BaseHook {
         uint256 maxWithdrawAmountToken0 = (amount0 * userShare) / 1e18;
         uint256 maxWithdrawAmountToken1 = (amount1 * userShare) / 1e18;
 
-        require(amount0 <= maxWithdrawAmountToken0 && amount1 <= maxWithdrawAmountToken1);
+        require(
+            amount0 <= maxWithdrawAmountToken0 &&
+                amount1 <= maxWithdrawAmountToken1
+        );
         govToken.burn(msg.sender, amount0);
         govToken.burn(msg.sender, amount1);
-        
+
         uint256 withdrawAmountToken0 = _withdrawFromStrategy(
             currentActiveStrategyId,
             key.currency0,
@@ -297,8 +252,14 @@ contract JITHook is BaseHook {
             key.currency1,
             amount1
         );
-        ERC20(Currency.unwrap(key.currency0)).transfer(msg.sender, withdrawAmountToken0);
-        ERC20(Currency.unwrap(key.currency1)).transfer(msg.sender, withdrawAmountToken1);
+        ERC20(Currency.unwrap(key.currency0)).transfer(
+            msg.sender,
+            withdrawAmountToken0
+        );
+        ERC20(Currency.unwrap(key.currency1)).transfer(
+            msg.sender,
+            withdrawAmountToken1
+        );
 
         // todo calculate token amount for the user, and transfer to user
         // todo multiple user wil store funds here, when one user call this only his liquidty should be removed and transferred to user NOT ALL
@@ -330,13 +291,14 @@ contract JITHook is BaseHook {
     }
 
     function _getBalanceFromStrategy(
-        uint256 _id, 
-        Currency _currency0,
-        Currency _currency1
+        uint256 _id,
+        PoolKey calldata key
     ) internal returns (uint256 balanceOfToken0, uint256 balanceOfToken1) {
-        address token0 = Currency.unwrap(_currency0);
-        address token1 = Currency.unwrap(_currency1);
-        (balanceOfToken0, balanceOfToken1) = IStrategy(controller.getStrategyAddress(_id)).getBalance(token0, token1);
+        address token0 = Currency.unwrap(key.currency0);
+        address token1 = Currency.unwrap(key.currency1);
+        (balanceOfToken0, balanceOfToken1) = IStrategy(
+            controller.getStrategyAddress(_id)
+        ).getBalance(token0, token1);
     }
 
     function _addLiquidityToPool(
@@ -353,7 +315,7 @@ contract JITHook is BaseHook {
         );
         bytes[] memory params = new bytes[](2);
 
-        (,int24 currTick,,) = StateLibrary.getSlot0(poolManager, key.toId());
+        (, int24 currTick, , ) = StateLibrary.getSlot0(poolManager, key.toId());
 
         uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
             TickMath.getSqrtPriceAtTick(currTick),
@@ -392,5 +354,111 @@ contract JITHook is BaseHook {
         delete currentPositionId;
 
         positionManager.modifyLiquiditiesWithoutUnlock(actions, params);
+    }
+
+    function _getSwapAmount(
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params
+    ) internal view returns (uint256 amountToSwap) {
+        uint256 precision;
+        int256 price;
+        int256 amountSpecified = params.amountSpecified;
+        uint256 token0Decimals = ERC20(Currency.unwrap(key.currency0))
+            .decimals();
+        uint256 token1Decimals = ERC20(Currency.unwrap(key.currency1))
+            .decimals();
+
+        if (params.zeroForOne) {
+            if (params.amountSpecified < 0) {
+                precision = 10 ** token0Decimals;
+                amountSpecified = -amountSpecified;
+                (, price, , , ) = priceFeeds[key.currency0].latestRoundData();
+            } else {
+                precision = 10 ** token1Decimals;
+                (, price, , , ) = priceFeeds[key.currency1].latestRoundData();
+            }
+        } else {
+            if (params.amountSpecified < 0) {
+                amountSpecified = -amountSpecified;
+                precision = 10 ** token1Decimals;
+                (, price, , , ) = priceFeeds[key.currency1].latestRoundData();
+            } else {
+                precision = 10 ** token0Decimals;
+                (, price, , , ) = priceFeeds[key.currency0].latestRoundData();
+            }
+        }
+
+        // this should be in USD (8 decimals)
+        amountToSwap = (uint256(amountSpecified) * uint256(price)) / precision;
+    }
+
+    function _checkIfHookHasEnoughBalance(
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params
+    ) internal returns (bool) {
+        (
+            uint256 token0Balance,
+            uint256 token1Balance
+        ) = _getBalanceFromStrategy(currentActiveStrategyId, key);
+
+        uint256 amountSpecified = params.amountSpecified < 0
+            ? uint256(-params.amountSpecified)
+            : uint256(params.amountSpecified);
+
+        // check if we have enough funds to cover the swap before adding liquidity
+    }
+
+    function _swap(
+        PoolKey calldata key
+    ) internal returns (uint256 amount0, uint256 amount1) {
+        (
+            uint256 token0Balance,
+            uint256 token1Balance
+        ) = _getBalanceFromStrategy(currentActiveStrategyId, key);
+        
+        (, int24 tick, , ) = StateLibrary.getSlot0(poolManager, key.toId());
+        uint160 sqrtPriceX96 = TickMath.getSqrtPriceAtTick(tick);
+        uint256 currentPoolPrice = (uint256(sqrtPriceX96) *
+            uint256(sqrtPriceX96) *
+            1e18) >> 192;
+
+        uint256 amountWithdrawn0 = _withdrawFromStrategy(
+            currentActiveStrategyId,
+            key.currency0,
+            token0Balance
+        );
+        uint256 amountWithdrawn1 = _withdrawFromStrategy(
+            currentActiveStrategyId,
+            key.currency1,
+            token1Balance
+        );
+
+        uint256 amount0 = (token1Balance * 1e18) / currentPoolPrice;
+        uint256 amount1 = (token0Balance * currentPoolPrice) / 1e18;
+
+        if (token0Balance > amount0) {
+            // swap token0 for token1
+            poolManager.swap(
+                key,
+                IPoolManager.SwapParams({
+                    zeroForOne: true,
+                    amountSpecified: int256(token0Balance - amount0),
+                    sqrtPriceLimitX96: 0
+                }),
+                ""
+            );
+        } else if (token1Balance > amount1) {
+            // swap token1 for token0
+            uint256 excessAmount1 = token1Balance - amount1;
+            poolManager.swap(
+                key,
+                IPoolManager.SwapParams({
+                    zeroForOne: false,
+                    amountSpecified: int256(excessAmount1),
+                    sqrtPriceLimitX96: 0
+                }),
+                ""
+            );
+        }
     }
 }
